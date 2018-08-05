4fcd1d1d8197bdd9a426362d93a10cc49d0c3198
amzhemu <33649552+amzhemu@users.noreply.github.com>
2018-08-05 16:20:16 +0800

    应用题1
    从顺序表中删除具有最小值得元素（假设唯一）并由函数返回被删元素的值，空出的位置由最后一个元素填补，若顺序表为空则显示出错信息并退出。

---
+++ seqlist_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include"slist.h"
+using namespace std;
+
+bool del_min(SeqList &L,ELemType &value)
+    {
+        if(L.length==0)
+            return false;
+        value=L.data[0];
+        int j=0;
+        for(int i=1; i<L.length; i++)
+        {
+            if(value>L.data[i])
+                value=L.data[i];
+            j=i;
+        }
+        L.data[j]=L.data[L.length-1];
+        L.length--;
+        return true;
+    }
